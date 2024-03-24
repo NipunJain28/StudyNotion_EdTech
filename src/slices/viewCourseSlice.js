3d37fdc5657624493c975427e0027a2722dbd0de
NipunJain28 <nipunofficial28@gmail.com>
2024-03-24 16:29:34 +0530

    First Commit StudyNotion

---
+++ src/slices/viewCourseSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  courseSectionData: [],
+  courseEntireData: [],
+  completedLectures: [],
+  totalNoOfLectures: 0,
+}
+
+const viewCourseSlice = createSlice({
+  name: "viewCourse",
+  initialState,
+  reducers: {
+    setCourseSectionData: (state, action) => {
+      state.courseSectionData = action.payload
+    },
+    setEntireCourseData: (state, action) => {
+      state.courseEntireData = action.payload
+    },
+    setTotalNoOfLectures: (state, action) => {
+      state.totalNoOfLectures = action.payload
+    },
+    setCompletedLectures: (state, action) => {
+      state.completedLectures = action.payload
+    },
+    updateCompletedLectures: (state, action) => {
+      state.completedLectures = [...state.completedLectures, action.payload]
+    },
+    resetViewCourse:(state)=>{
+      state.completedLectures=[];
+      state.courseEntireData=[];
+      state.courseSectionData=[];
+      state.totalNoOfLectures=0;
+    }
+  },
+})
+
+export const {
+  setCourseSectionData,
+  setEntireCourseData,
+  setTotalNoOfLectures,
+  setCompletedLectures,
+  updateCompletedLectures,
+} = viewCourseSlice.actions
+
+export default viewCourseSlice.reducer
